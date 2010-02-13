796d1ba61bf59efb5907b7884a4b1cfaa9f78bfe
Lukas Rist <glaslos@gmail.com>
2010-02-13 11:52:19 +0000

    comments added and port check improved

--- modules/result.py
+++ modules/result.py
@@ -12,11 +12,11 @@ def parse(report):
     CnC_NICK = ""
     CnC_USER = ""
     
-    #print report
     for line in report:
         #if "<fsockopen>" in line:
         if "host=" in line:
             CnC_IP = line.partition("host=")[2].partition(",")[0]
+        if "port=" in line:
             CnC_PORT = line.rpartition("port=")[2].partition(",")[0]
         #if "<fwrite>" in line and "JOIN" in line:
         if "JOIN" in line:
@@ -26,6 +26,7 @@ def parse(report):
         if "NICK" in line:
             CnC_NICK = line.partition("NICK")[2].partition("\n")[0].strip()
         if "USER" in line:
+            # sometimes there are errors while computing the username
             if ":Parse error:" in line or ":Fatal error:" in line:
                 CnC_USER = line.partition("USER")[2].rpartition(":")[0].strip() + " :" + CnC_NICK
             else:
