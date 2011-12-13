9521317b59a458d09eca96d5dd54d3a7744412a1
Guesslin <guesslin1986@gmail.com>
2011-12-13 10:59:13 +0000

    Add conver_db for adding three column in botnet_info.db file

---
+++ modules/convert_db.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+"""
+This script is for converting wsbs database to add three columns 
+with server_status, channel_status, and bot_status
+"""
+
+class convert_botnet_info_db(object):
+
+    def __init__(self):
+        self.conn = sqlite3.connect("../db/botnet_info.db")
+        self.convert_it()
+    
+    def convert_it(self):
+        cursor = self.conn.cursor()
+        schema = cursor.execute("""SELECT * FROM sqlite_master WHERE type='table' AND name='botnet_info'""").fetchone()
+        try:
+            if not 'server_status' in schema[4]:
+                print "Adding server_status"
+                cursor.execute("""ALTER TABLE botnet_info ADD COLUMN server_status TEXT""")
+            if not 'channel_status' in schema[4]:
+                print "Adding channel_status"
+                cursor.execute("""ALTER TABLE botnet_info ADD COLUMN channel_status TEXT""")
+            if not 'bot_status' in schema[4]:
+                print "Adding bot_status"
+                cursor.execute("""ALTER TABLE botnet_info ADD COLUMN bot_status TEXT""")
+        except Exception as e:
+            print e
+        self.conn.commit()
+        cursor.close()
+        self.conn.close()
+
+cdb = convert_botnet_info_db()
+
